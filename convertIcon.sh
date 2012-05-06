9db81b2d0c00f2a52be356a7a4f4387a4ce332b3
Jeroen Wyseur <jeroen.wyseur@gmail.com>
2012-05-06 13:10:30 +0200

    inkscape scripts

---
+++ convertIcon.sh
@@ -0,0 +1,4 @@
+inkscape $1.svg --export-png=drawable-ldpi/$1.png -w24 -h24
+inkscape $1.svg --export-png=drawable-mdpi/$1.png -w32 -h32
+inkscape $1.svg --export-png=drawable-hdpi/$1.png -w48 -h48
+
---
+++ convertLauncher.sh
@@ -0,0 +1,5 @@
+inkscape $1.svg --export-png=drawable-ldpi/$1.png -w36 -h36
+inkscape $1.svg --export-png=drawable-mdpi/$1.png -w48 -h48
+inkscape $1.svg --export-png=drawable-hdpi/$1.png -w72 -h72
+inkscape $1.svg --export-png=drawable-xhdpi/$1.png -w96 -h96
+inkscape $1.svg --export-png=$1.png -w512 -h512
