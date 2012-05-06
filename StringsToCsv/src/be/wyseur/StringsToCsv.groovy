f02525f293df508f08b8386f7acb517bc7dbbbd2
Jeroen Wyseur <jeroen.wyseur@gmail.com>
2012-05-06 14:56:46 +0200

    Multiple conversions from strings to csv

--- StringsToCsv/src/be/wyseur/StringsToCsv.groovy
+++ StringsToCsv/src/be/wyseur/StringsToCsv.groovy
@@ -1,17 +1,51 @@
 package be.wyseur
 
 def separator = ";"
-def file = new XmlParser().parse(new File("D:\\projects\\Git\\photoframeKobe\\PhotoFrame\\res\\values\\strings.xml"));
-new File("keys.csv").withWriter { out ->
-	out.writeLine "key" + separator + "en"
-	file.string.each { 
-		out.writeLine it.@name + separator + it.text()
-	}
-	file["string-array"].each{
-		def arr = it.@name
-		def i =0;
-		it.item.each{
-			out.writeLine arr + "." + (i++) + separator + it.text()
+def langs = []
+def values = [:]
+
+def folder = new File("D:\\projects\\Git\\photoframeKobe\\PhotoFrame\\res\\")
+folder.eachFile() { file->  
+	if (file.name.startsWith("values")){
+		def parts = file.name.tokenize('-')
+		def lang = "en"
+		if (parts.size() > 1){
+			lang = parts[1]
 		}
+		println("Language " + lang)
+		langs.add(lang)
+		def xml = new XmlParser().parse(new File(file, "strings.xml"));
+		xml.string.each {
+			if (!values[it.@name]){
+				values[it.@name] = []
+			}
+			values[it.@name].add(it.text())
+		}
+		xml["string-array"].each{
+			def arr = it.@name
+			def i =0;
+			it.item.each{
+				def key = arr + "." + (i++)
+				if (!values[key]){
+					values[key] = []
+				}
+				values[key].add(it.text())
+			}
+		}
+	}
+}  
+
+new File("keys.csv").withWriter { out ->
+	out.write "key" 
+	langs.each{
+		out.write separator + it
+	}
+	out.write "\n"
+	values.each {
+		out.write it.key
+		it.value.each{
+			out.write separator + it
+		}
+		out.write "\n"
 	}
 }
